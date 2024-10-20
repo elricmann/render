025fae6c6b6506569adba111d4562c2a7cb8040e
elricmann <elricmann@proton.me>
2024-10-20 18:14:41 +0300

    doc: patch

--- src/patch.ts
+++ src/patch.ts
@@ -5,102 +5,110 @@ import {
   OPCODE_TEXT_NODE,
 } from "./vm";
 
+/**
+ * Applies instruction-level patching of bytecodes
+ * while iterating through strings and other structures
+ *
+ * @param prevBytecode old bytecode instance
+ * @param nextBytecode new bytecode instance
+ * @returns patched bytecode after merging
+ */
 export function patch(
-  oldBytecode: Uint8Array,
-  newBytecode: Uint8Array
+  prevBytecode: Uint8Array,
+  nextBytecode: Uint8Array
 ): Uint8Array {
   const patchedBytecode: number[] = [];
 
-  let oldIndex = 0;
-  let newIndex = 0;
+  let prevIndex = 0;
+  let nextIndex = 0;
 
-  while (oldIndex < oldBytecode.length && newIndex < newBytecode.length) {
-    const oldOpcode = oldBytecode[oldIndex];
-    const newOpcode = newBytecode[newIndex];
+  while (prevIndex < prevBytecode.length && nextIndex < nextBytecode.length) {
+    const prevOpcode = prevBytecode[prevIndex];
+    const nextOpcode = nextBytecode[nextIndex];
 
-    if (oldOpcode === newOpcode) {
+    if (prevOpcode === nextOpcode) {
       // same opcode, copy to the patched bytecode
-      patchedBytecode.push(newOpcode);
+      patchedBytecode.push(nextOpcode);
 
-      switch (newOpcode) {
+      switch (nextOpcode) {
         case OPCODE_CREATE_ELEMENT:
-          const oldTagNameLength = oldBytecode[oldIndex + 1];
-          const newTagNameLength = newBytecode[newIndex + 1];
+          const prevTagNameLength = prevBytecode[prevIndex + 1];
+          const nextTagNameLength = nextBytecode[nextIndex + 1];
 
           // if tag names are the same, copy each over
-          if (oldTagNameLength === newTagNameLength) {
-            for (let i = 0; i <= newTagNameLength; i++) {
-              patchedBytecode.push(newBytecode[newIndex + i]);
+          if (prevTagNameLength === nextTagNameLength) {
+            for (let i = 0; i <= nextTagNameLength; i++) {
+              patchedBytecode.push(nextBytecode[nextIndex + i]);
             }
           } else {
             // if the tag names differ, use the new tag name
-            patchedBytecode.push(newTagNameLength);
+            patchedBytecode.push(nextTagNameLength);
 
-            for (let i = 0; i < newTagNameLength; i++) {
-              patchedBytecode.push(newBytecode[newIndex + 2 + i]);
+            for (let i = 0; i < nextTagNameLength; i++) {
+              patchedBytecode.push(nextBytecode[nextIndex + 2 + i]);
             }
           }
 
-          oldIndex += oldTagNameLength + 2;
-          newIndex += newTagNameLength + 2;
+          prevIndex += prevTagNameLength + 2;
+          nextIndex += nextTagNameLength + 2;
 
           break;
 
         case OPCODE_SET_ATTRIBUTE:
           // force same idx on both old and new attributes
-          const oldAttrLength = oldBytecode[oldIndex + 1];
-          const newAttrLength = newBytecode[newIndex + 1];
+          const prevAttrLength = prevBytecode[prevIndex + 1];
+          const nextAttrLength = nextBytecode[nextIndex + 1];
 
           // copy the attribute setting operation from the new bytecode
-          patchedBytecode.push(newAttrLength);
+          patchedBytecode.push(nextAttrLength);
 
-          for (let i = 0; i < newAttrLength; i++) {
-            patchedBytecode.push(newBytecode[newIndex + 2 + i]);
+          for (let i = 0; i < nextAttrLength; i++) {
+            patchedBytecode.push(nextBytecode[nextIndex + 2 + i]);
           }
 
-          oldIndex += oldAttrLength + 2;
-          newIndex += newAttrLength + 2;
+          prevIndex += prevAttrLength + 2;
+          nextIndex += nextAttrLength + 2;
 
           break;
 
         case OPCODE_TEXT_NODE:
           // compare text content, and patch if different
-          const oldTextLength = oldBytecode[oldIndex + 1];
-          const newTextLength = newBytecode[newIndex + 1];
+          const prevTextLength = prevBytecode[prevIndex + 1];
+          const nextTextLength = nextBytecode[nextIndex + 1];
 
-          if (oldTextLength === newTextLength) {
-            for (let i = 0; i < newTextLength; i++) {
-              patchedBytecode.push(newBytecode[newIndex + 2 + i]);
+          if (prevTextLength === nextTextLength) {
+            for (let i = 0; i < nextTextLength; i++) {
+              patchedBytecode.push(nextBytecode[nextIndex + 2 + i]);
             }
           } else {
-            patchedBytecode.push(newTextLength);
+            patchedBytecode.push(nextTextLength);
 
-            for (let i = 0; i < newTextLength; i++) {
-              patchedBytecode.push(newBytecode[newIndex + 2 + i]);
+            for (let i = 0; i < nextTextLength; i++) {
+              patchedBytecode.push(nextBytecode[nextIndex + 2 + i]);
             }
           }
 
-          oldIndex += oldTextLength + 2;
-          newIndex += newTextLength + 2;
+          prevIndex += prevTextLength + 2;
+          nextIndex += nextTextLength + 2;
 
           break;
 
         default:
-          oldIndex++;
-          newIndex++;
+          prevIndex++;
+          nextIndex++;
 
           break;
       }
     } else {
       // if the opcodes differ, use the new one (representing the diff)
-      patchedBytecode.push(newOpcode);
-      newIndex++; // move to the next opcode in new bytecode
+      patchedBytecode.push(nextOpcode);
+      nextIndex++; // move to the next opcode in new bytecode
     }
   }
 
   // if there are leftover new bytecodes, add them to patched
-  while (newIndex < newBytecode.length) {
-    patchedBytecode.push(newBytecode[newIndex++]);
+  while (nextIndex < nextBytecode.length) {
+    patchedBytecode.push(nextBytecode[nextIndex++]);
   }
 
   return new Uint8Array(patchedBytecode);
